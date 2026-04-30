e55cddfc2ce4d2b5b3c836ab9a2701c7909e9c22
stippy-sudo <stippyshops@gmail.com>
2026-04-29 18:42:10 -0700

    Update variables-naming-conventions-and-top-to-bottom-code-evaluation.js
    first

--- variables-naming-conventions-and-top-to-bottom-code-evaluation.js
+++ variables-naming-conventions-and-top-to-bottom-code-evaluation.js
@@ -24,9 +24,9 @@ Things to reflect on:
   
 */
 
-let a = "Alice";
-let b = 5;
-let c = 20;
-let d = a + " bought " + b + " items for $" + c + ".";
+let name = "Alice";
+let numberItems = 5;
+let total = 20;
+let summary = name + " bought " + numberItems + " items for $" + total + ".";
 
 console.log(d);
